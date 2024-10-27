fb50d06ea3b53f2ac4c5f251797f1b4b115acf8c
CatGamer7 <arthur.zayda@gmail.com>
2024-10-27 18:30:28 +0700

    update: parser cli

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
+import argparse
 import csv
 from parser.parser import Extended_Reddit_RO
 from parser.data_model import CSV_Model
 
 
-if __name__ == "__main__":
-    
-    with open("name.csv", "w", newline="", encoding="UTF-8") as file:
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-o", "--output", help="Output file name", required=True)
+    parser.add_argument("-cid", "--client_id", help="Client id of reddit app", required=True)
+    parser.add_argument("-cs", "--client_secret", help="Client secret of reddit app", required=True)
+    parser.add_argument("-u", "--user_agent", help="User agent of reddit app", required=True)
+    parser.add_argument("-s", "--subreddit", help="Subreddit display name to parse", required=True)
+    parser.add_argument("-f", "--flairs", nargs='+', help="Space separated flair names")
+    parser.add_argument("-l", "--limit", help="Parse limit on number of submissions", default=10000)
+
+    args = parser.parse_args()
+
+    filename = args.output if args.output else "output.csv"
+
+    with open(filename, "w", newline="", encoding="UTF-8") as file:
         writer = csv.writer(file)
 
         parser = Extended_Reddit_RO(
-            "abcFZ-aLBO94KN428uLX7A",
-            "gesLnro3-NaqL_blXaHowIHRpfz3Hw",
-            "english comment scraper (by u/Holiday_Scarcity_911)"
+            args.client_id,
+            args.client_secret,
+            args.user_agent
         )
         nodes = parser.parse_subreddit(
-            "srilanka",
-            ["Politics", "News", "Discussion", "Beaurocracy", "Rumour"],
-            CSV_Model
+            args.subreddit,
+            args.flairs,
+            CSV_Model,
+            args.limit
         )
 
         writer.writerow(CSV_Model.TUPLE_HEADERS)
         for node in nodes:
             writer.writerow(node.export())
+
+if __name__ == "__main__":
+    main()    
--- parser/parser.py
+++ parser/parser.py
@@ -10,8 +10,7 @@ class Extended_Reddit_RO(Reddit):
 
     def __init__(self, client_id: str, client_secret: str, user_agent: str,
                  username: str | None = None, password: str | None = None,
-                 ratelimit_seconds: int = 3600, application_level_retries:
-                 int = 3
+                 ratelimit_seconds: int = 3600
         ):
 
         """Parameters:
@@ -43,6 +42,7 @@ class Extended_Reddit_RO(Reddit):
     def parse_subreddit(
             self, subreddit_display_name: str,
             flairs: list[str], Model: type[Base_Model] = CSV_Model,
+            parse_limit: int = 10000,
             replace_limit: int = None
         ) -> Generator[Base_Model]:
 
@@ -56,7 +56,7 @@ class Extended_Reddit_RO(Reddit):
         search_query = " OR ".join(prefix_flairs)
         subreddit = self.subreddit(subreddit_display_name)
 
-        for submission in subreddit.search(search_query, limit=3):
+        for submission in subreddit.search(search_query, limit=parse_limit):
 
             submission_node = Model(
                 submission.fullname,
